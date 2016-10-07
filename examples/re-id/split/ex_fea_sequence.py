77c2c3475d718a3e3f68e71d90d61d3f93e0b0c9
daodaofr <wodaodao@gmail.com>
2016-10-07 15:52:50 +0800

    Update ex_fea_sequence.py

--- examples/re-id/split/ex_fea_sequence.py
+++ examples/re-id/split/ex_fea_sequence.py
@@ -12,40 +12,19 @@ import h5py
 import random
 from scipy.io import loadmat
 
-def initialize_transformer(image_mean, is_flow):
-  shape = (10, 3, 160, 60)
-  transformer = caffe.io.Transformer({'data': shape})
-  channel_mean = np.zeros((3,160,60))
-  for channel_index, mean_val in enumerate(image_mean):
-    channel_mean[channel_index, ...] = mean_val
-  transformer.set_mean('data', channel_mean)
-  transformer.set_raw_scale('data', 255)
-  transformer.set_channel_swap('data', (2, 1, 0))
-  transformer.set_transpose('data', (2, 0, 1))
-  transformer.set_is_flow('data', is_flow)
-  return transformer
-
-RGB_frames = '/media/yanyichao/5A487B64487B3DB7/re_id/prid/data_lstm/'
-color_path = '/media/yanyichao/5A487B64487B3DB7/re_id/prid/fea/lbp_color/'
+color_path = 'lbp_color_fea'
 fea_dim = 58950
-#RGB_frames = '/home/yanyichao/data/cub_org/train_test_filter_new_org_sort_vgg_bbox/'
-ucf_mean_RGB = np.zeros((3,1,1))
-#ucf_mean_flow = np.zeros((3,1,1))
-#ucf_mean_flow[:,:,:] = 128
-ucf_mean_RGB[0,:,:] = 104
-ucf_mean_RGB[1,:,:] = 117
-ucf_mean_RGB[2,:,:] = 123
-transformer_RGB = initialize_transformer(ucf_mean_RGB, False)
-
 lstm_model = 'deploy_lstm.prototxt'
-RGB_lstm = 'models/_iter_25000.caffemodel'
+RGB_lstm = 're-id.caffemodel'
 RGB_lstm_net =  caffe.Net(lstm_model, RGB_lstm, caffe.TEST)
 
-def LRCN_ex_fea(frames_sequence, net, transformer,mat_path, num_frames, is_flow):
+def LRCN_ex_fea(net, mat_path):
   clip_length = 10
   offset = 1
   output_predictions = np.zeros((clip_length,512))
   color_fea_input = loadmat(mat_path)
+  mat1=color_fea_input['tmp_fea']
+  num_frames = mat1.shape[0]
   color_fea_r = color_fea_input['tmp_fea']
   caffe_in = np.zeros((clip_length, fea_dim))
   clip_clip_markers = np.ones((clip_length,1,1,1))
@@ -68,21 +47,18 @@ f.close()
 true_pred = 0
 all_test = 0
 all_fea = np.zeros((len(f_lines), 10,512))
-itr = 20
+itr = 1
 for it in range(itr):
     for ix, line in enumerate(f_lines):
         video = line.split(' ')[0]
         l = int(line.split(' ')[1])
         video1 = line.split(' ')[0].split('/')[1]
-        frames = glob.glob('%s%s/*.png' %(RGB_frames, video))
-        num_frames = len(frames)
-        frames_sequence = frames[0][0:-9] + '_%04d.png'
         color_mat_path = color_path+video1+'.mat'
         print "processing the %d th image" % ix
         tmp_fea = \
-             LRCN_ex_fea(frames_sequence, RGB_lstm_net, transformer_RGB,color_mat_path, num_frames, False)
-    #np.save("/home/yanyichao/data/cub_org/list/test_ss_new/tmp_fea.npy",tmp_fea)
+             LRCN_ex_fea( RGB_lstm_net, color_mat_path)
         all_fea[ix] = tmp_fea
     f_all = h5py.File('train_25k_'+str(it)+'.h5', "w")
     f_all.create_dataset('train_set', data = all_fea)
     f_all.close()
+
